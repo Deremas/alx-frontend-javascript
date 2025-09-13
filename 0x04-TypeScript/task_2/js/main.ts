a943f432ff46531866b9bc13493ece79bf13b85b
Dereje Masresha <derejemasresha27@gmail.com>
2025-09-13 19:10:10 +0300

    task 2 main.ts refined

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -50,64 +50,27 @@ function createEmployee(salary: number | string): Director | Teacher {
   }
 }
 
+// Type guard function
+export function isDirector(employee: Director | Teacher): employee is Director {
+  return employee instanceof Director;
+}
+
+// executeWork function
+export function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
+}
+
 // Expected results
 console.log(createEmployee(200));
 console.log(createEmployee(1000));
 console.log(createEmployee("$500"));
 
-// // Interfaces
-// interface DirectorInterface {
-//   workFromHome(): string;
-//   getCoffeeBreak(): string;
-//   workDirectorTasks(): string;
-// }
-
-// interface TeacherInterface {
-//   workFromHome(): string;
-//   getCoffeeBreak(): string;
-//   workTeacherTasks(): string;
-// }
-
-// // Director Class
-// class Director implements DirectorInterface {
-//   workFromHome(): string {
-//     return "Working from home";
-//   }
-
-//   getCoffeeBreak(): string {
-//     return "Getting a coffee break";
-//   }
-
-//   workDirectorTasks(): string {
-//     return "Getting to director tasks";
-//   }
-// }
-
-// // Teacher Class
-// class Teacher implements TeacherInterface {
-//   workFromHome(): string {
-//     return "Cannot work from home";
-//   }
-
-//   getCoffeeBreak(): string {
-//     return "Cannot have a break";
-//   }
-
-//   workTeacherTasks(): string {
-//     return "Getting to work";
-//   }
-// }
-
-// // createEmployee function
-// function createEmployee(salary: number | string): Director | Teacher {
-//   if (typeof salary === "number" && salary < 500) {
-//     return new Teacher();
-//   } else {
-//     return new Director();
-//   }
-// }
-
-// // Expected results
-// console.log(createEmployee(200));
-// console.log(createEmployee(1000));
-// console.log(createEmployee("$500"));
+// Demonstration of isDirector and executeWork
+console.log(isDirector(createEmployee(200)));
+console.log(isDirector(createEmployee(1000)));
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
