86328577ded432613d3b262099a30942445b38fd
Dereje Masresha <derejemasresha27@gmail.com>
2025-09-13 19:37:38 +0300

    crud.d.ts refined

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,24 +1,5 @@
 import { RowID, RowElement } from "./interface";
 
-// Declare a global namespace for the CRUD functions
-declare namespace CRUD {
-  export function insertRow(row: RowElement): RowID;
-  export function deleteRow(rowId: RowID): void;
-  export function updateRow(rowId: RowID, row: RowElement): RowID;
-}
-// // crud.js provided by the user
-// export function insertRow(row) {
-//   console.log("Insert row", row);
-//   return Math.floor(Math.random() * Math.floor(1000));
-// }
-
-// export function deleteRow(rowId) {
-//   console.log("Delete row id", rowId);
-//   return;
-// }
-
-// export function updateRow(rowId, row) {
-//   console.log(`Update row ${rowId}`, row);
-
-//   return rowId;
-// }
+declare function insertRow(row: RowElement): RowID;
+declare function deleteRow(rowId: RowID): void;
+declare function updateRow(rowId: RowID, row: RowElement): RowID;
