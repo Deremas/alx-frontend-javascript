422c014ac4419aac6fabb9d31b6e50c5d5df6277
Dereje Masresha <derejemasresha27@gmail.com>
2025-09-13 19:26:14 +0300

    todayClass added to task 2

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -90,3 +90,19 @@ function teachClass(todayClass: Subjects): string {
 // Expected results for teachClass
 console.log(teachClass("Math"));
 console.log(teachClass("History"));
+
+// String literal type
+type Subjects = "Math" | "History";
+
+// teachClass function
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === "Math") {
+    return "Teaching Math";
+  } else if (todayClass === "History") {
+    return "Teaching History";
+  }
+}
+
+// Expected results for teachClass
+console.log(teachClass("Math"));
+console.log(teachClass("History"));
