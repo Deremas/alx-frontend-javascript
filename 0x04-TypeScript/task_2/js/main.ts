962b405fc321a2342ef065ee10eed9273f3c30cb
Dereje Masresha <derejemasresha27@gmail.com>
2025-09-13 19:08:27 +0300

    task 2 main.ts refined

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -43,7 +43,7 @@ class Teacher implements TeacherInterface {
 
 // createEmployee function
 function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === "number" && salary < 500) {
+  if (salary < 500) {
     return new Teacher();
   } else {
     return new Director();
@@ -54,3 +54,60 @@ function createEmployee(salary: number | string): Director | Teacher {
 console.log(createEmployee(200));
 console.log(createEmployee(1000));
 console.log(createEmployee("$500"));
+
+// // Interfaces
+// interface DirectorInterface {
+//   workFromHome(): string;
+//   getCoffeeBreak(): string;
+//   workDirectorTasks(): string;
+// }
+
+// interface TeacherInterface {
+//   workFromHome(): string;
+//   getCoffeeBreak(): string;
+//   workTeacherTasks(): string;
+// }
+
+// // Director Class
+// class Director implements DirectorInterface {
+//   workFromHome(): string {
+//     return "Working from home";
+//   }
+
+//   getCoffeeBreak(): string {
+//     return "Getting a coffee break";
+//   }
+
+//   workDirectorTasks(): string {
+//     return "Getting to director tasks";
+//   }
+// }
+
+// // Teacher Class
+// class Teacher implements TeacherInterface {
+//   workFromHome(): string {
+//     return "Cannot work from home";
+//   }
+
+//   getCoffeeBreak(): string {
+//     return "Cannot have a break";
+//   }
+
+//   workTeacherTasks(): string {
+//     return "Getting to work";
+//   }
+// }
+
+// // createEmployee function
+// function createEmployee(salary: number | string): Director | Teacher {
+//   if (typeof salary === "number" && salary < 500) {
+//     return new Teacher();
+//   } else {
+//     return new Director();
+//   }
+// }
+
+// // Expected results
+// console.log(createEmployee(200));
+// console.log(createEmployee(1000));
+// console.log(createEmployee("$500"));
